40aca62dc43a4584d8ffd4121ed2c362154af07c
Arkadazzz <157562858+Arkadazzz@users.noreply.github.com>
2026-09-04 23:34:57 -0400

    Update panel.js

--- panel.js
+++ panel.js
@@ -16,11 +16,9 @@ const auth = getAuth(app);
 const db = getDatabase(app);
 
 const CORREOS_ADMINISTRADORES = [
-    "cami.fevreseguel@gmail.com",
+    "nat.producciones2020@gmail.com",
     "pinoelgueta@gmail.com", 
-    "natalyseguel.va@gmail.com",
-    "javier.rojas.fer@gmail.com",
-    "luisemilio.jorquera.avaria@gmail.com",
+    "correo_jefa_1@gmail.com"
 ];
 
 onAuthStateChanged(auth, (user) => { 
